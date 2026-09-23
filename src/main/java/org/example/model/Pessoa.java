72cc6af49a31f41f5b8d46f990b4ba1340c52c97
erick-dias <erickdias0411@gmail.com>
2026-09-23 09:19:06 -0300

    feat:  classe Pessoa

---
+++ src/main/java/org/example/model/Pessoa.java
@@ -0,0 +1,12 @@
+public class Pessoa {
+
+    private String nome;
+    private LocalDate dataNascimento;
+
+    public Pessoa(String nome, LocalDate dataNascimento) {
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+    }
+
+    // getters e setters
+}
